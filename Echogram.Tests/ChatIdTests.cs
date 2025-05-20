6cc2813d3fc2d2da75d237c18e437f9dcfa849d4
Vladislav Borovikov <vborovikov@live.ru>
2025-05-20 21:11:27 +0500

    created test project

---
+++ Echogram.Tests/ChatIdTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Echogram.Tests;
+
+using Echo.Telegram;
+
+[TestClass]
+public sealed class ChatIdTests
+{
+    [DataTestMethod]
+    [DataRow("@channelusername", "@channelusername")]
+    [DataRow("channelusername", "@channelusername")]
+    [DataRow("123", "123")]
+    [DataRow(123, "123")]
+    public void ConvertFrom_IdOrName_Converted(object identity, string str)
+    {
+        var chatId = ChatId.ConvertFrom(identity);
+
+        Assert.AreNotEqual(default, chatId);
+        Assert.AreEqual(str, chatId.ToString());
+    }
+}
